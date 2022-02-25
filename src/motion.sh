380875e70f740697c97f3a263321f25b96f42f9d
alexandrzlobin <alexandrzlobin@gmail.com>
2022-02-25 22:05:05 +0300

    delete .sh files

--- src/motion.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Launch the robot
-source /home/zlo/robodog/devel/setup.bash
-
-echo "spot_micro_keyboard_command keyboard_command.launch, please wait!"
-roslaunch spot_micro_keyboard_command keyboard_command.launch run_rviz:=true
--- src/slam.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo chmod 666 /dev/ttyUSB0
-# Launch the robot
-source /home/zlo/robodog/devel/setup.bash 
-
-echo "Launching motion_control_and_hector_slam.launch, please wait!"
-roslaunch spot_micro_launch motion_control_and_hector_slam.launch
-
